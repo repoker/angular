5b7f09a03733b9ef273870e1c601db554dd1c05b
Pau Pineda <pinedarp@diba.cat>
2020-01-22 19:08:48 +0100

    Más mierda

--- proyectos-angular/02-PLACEHOLDER/src/app/components/body/body.component.ts
+++ proyectos-angular/02-PLACEHOLDER/src/app/components/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-body',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
-  constructor() { }
+  frase: any = {
+    autor: 'Homer Simpson',
+    mensaje: 'Tendrá todo el dinero del mundo, pero hay algo que jamás podrá comprar: un dinosaurio'
+  };
+
+  mostrar = false;
+
+  colores: string[] = ['green', 'red', 'blue', 'brown', 'white'];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  toggleButton() {
+    this.mostrar = !this.mostrar;
+  }
+
+  randomizeColors() {
+
+    var currentIndex = this.colores.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = this.colores[currentIndex];
+      this.colores[currentIndex] = this.colores[randomIndex];
+      this.colores[randomIndex] = temporaryValue;
+    }
+  }
 }
