72a0ec668c39ec6874d41393aa1906ff54ce28d9
Pau Pineda <pinedarp@diba.cat>
2020-02-05 21:49:34 +0100

    Inicio tictactoe

--- proyectos-angular/tictactoe/src/app/app.module.ts
+++ proyectos-angular/tictactoe/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {GameModule} from './game/game.module';
+import {GameComponent} from './game/game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GameModule
   ],
   providers: [],
   bootstrap: [AppComponent]
